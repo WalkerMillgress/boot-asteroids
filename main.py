6ce4ac15dfd7cec32d9fb982fa72073b83924290
Walker M. <walker@1ks.ink>
2025-05-09 13:14:25 -0300

    now you can crash into things

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import pygame
 from constants import *
 from player import Player
 from circleshape import CircleShape
+from asteroid import Asteroid
+from asteroidfeild import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     clock = pygame.time.Clock()
     dt = 0
 
@@ -18,8 +27,18 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        
+        for drawed in drawable:
+            drawed.draw(screen)
+
+
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
+                print("Game over!")
+                return
+
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
